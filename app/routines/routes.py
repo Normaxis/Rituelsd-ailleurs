f6f0ada75bde680714311a4f62f4eb88ac3f18c0
Normaxis <lefebvre.flavien2018@gmail.com>
2026-06-26 22:51:45 +0200

    Autorise routines generales sans cabine

--- app/routines/routes.py
+++ app/routines/routes.py
@@ -9,7 +9,10 @@ routines_bp = Blueprint('routines', __name__)
 @login_required
 def index():
     if request.method == 'POST':
-        r = Routine(name=request.form['name'], cabin_id=int(request.form['cabin_id']), instructions=request.form['instructions'])
-        db.session.add(r); db.session.commit()
+        cabin_value = request.form.get('cabin_id') or ''
+        cabin_id = int(cabin_value) if cabin_value else None
+        r = Routine(name=request.form['name'], cabin_id=cabin_id, instructions=request.form.get('instructions',''))
+        db.session.add(r)
+        db.session.commit()
         return redirect(url_for('routines.index'))
-    return render_template('routines/index.html', routines=Routine.query.all(), cabins=Cabin.query.all())
+    return render_template('routines/index.html', routines=Routine.query.order_by(Routine.name).all(), cabins=Cabin.query.order_by(Cabin.name).all())
